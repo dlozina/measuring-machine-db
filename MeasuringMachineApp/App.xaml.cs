11f630935d88ae6fc551f530d8d269ab13aa710a
Dino <dino.lozina2017@gmail.com>
2018-11-21 08:31:12 +0100

    firstCommit

--- MeasuringMachineApp/App.xaml.cs
+++ MeasuringMachineApp/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
+using PLCInterface;
 
 namespace MeasuringMachineApp
 {
@@ -13,5 +15,34 @@ namespace MeasuringMachineApp
     /// </summary>
     public partial class App : Application
     {
+        public static PLCInterface.Interface PlcInterface;
+        public static MainWindow mwHandle;
+
+        public App()
+        {
+            //PlcInterface = new PLCInterface.Interface();
+            PlcInterface = new Interface();
+            PlcInterface.StartCyclic(); // Possible system null reference
+            PlcInterface.Update_Online_Flag += new Interface.OnlineMarker(PLCInterface_PLCOnlineChanged);
+            PlcInterface.Update_100_ms += new Interface.UpdateHandler(PLC_Update_100_ms);
+        }
+
+        private void PLC_Update_100_ms(Interface sender, InterfaceEventArgs e)
+        {
+
+        }
+        private void PLCInterface_PLCOnlineChanged(object sender, OnlineMarkerEventArgs e)
+        {
+            //if (e.OnlineMark)
+            //{
+            //    mwHandle.onlineFlag.Dispatcher.BeginInvoke((Action)(() => { mwHandle.onlineFlag.Fill = new LinearGradientBrush(Colors.Green, Colors.White, 0.0); }));
+            //    mwHandle.t_connectionStatus.Dispatcher.BeginInvoke((Action)(() => { mwHandle.t_connectionStatus.Text = "PLC Status: Online"; }));
+            //}
+            //else
+            //{
+            //    mwHandle.onlineFlag.Dispatcher.BeginInvoke((Action)(() => { mwHandle.onlineFlag.Fill = new LinearGradientBrush((Color)ColorConverter.ConvertFromString("#FF979797"), Colors.White, 0.0); }));
+            //    mwHandle.t_connectionStatus.Dispatcher.BeginInvoke((Action)(() => { mwHandle.t_connectionStatus.Text = "PLC Status: Offline"; }));
+            //}
+        }
     }
 }
